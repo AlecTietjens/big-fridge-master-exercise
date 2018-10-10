7c9e5a94fcb49d5c8c2c146f446e981d0cc264db
Alec Tietjens <alectietjens@gmail.com>
2018-10-09 23:49:22 -0700

    server.js fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/big-fridge-master'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/big-fridge-master/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
